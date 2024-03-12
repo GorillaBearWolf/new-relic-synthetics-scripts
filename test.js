046da3eedc4c3398ad2256e184ff7e4c1e21f8dc
GorillaBearWolf <mrefsnider@icloud.com>
2024-03-12 19:48:37 -0400

    🍱 Add test.js file

---
+++ test.js
@@ -0,0 +1,24 @@
+﻿var request = require('request');
+
+var domain = '{url}';
+
+var options = {
+    'method': 'GET',
+    'url': 'https://www.rdap.net/domain/' + domain
+};
+
+request(options, function(error, response) {
+    if (error) throw new Error(error);
+    var newBody = JSON.parse(response.body);
+    var expDate = newBody.events[1].eventDate;
+    var newExp = Date.parse(expDate);
+
+    var now = Date.now();
+    var daysToExp = (newExp - now) / 86400000;
+
+    $util.insights.set("checkedDomain", domain);
+    $util.insights.set("domainExpirationDate", newExp);
+    $util.insights.set("daysToExpiration", daysToExp);
+
+    console.log(expDate + '\n' + newExp + '\n' + now + '\n' + daysToExp);
+});
