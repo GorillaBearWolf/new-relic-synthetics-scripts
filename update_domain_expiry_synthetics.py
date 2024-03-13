f6a077978aa080f6e8675e0be8d1dc9072e6b6db
GorillaBearWolf <mrefsnider@icloud.com>
2024-03-12 21:00:49 -0400

    ✨ Implemented REST API for update request // Implemented dynamic queries // Improved response messaging // Implemented read from javascript file to create payload

--- update_domain_expiry_synthetics.py
+++ update_domain_expiry_synthetics.py
@@ -14,40 +14,45 @@ import synthetics_functions
 def main():
     load_dotenv()
 
-    url_file = "test_urls.txt"
-    graph_url = "https://api.newrelic.com/graphql"
     api_key = os.environ.get("NEW_RELIC_API_KEY")
     headers = {"X-Api-Key": api_key, "Content-Type": "application/json"}
+    graph_url = "https://api.newrelic.com/graphql"
+    rest_url = "https://synthetics.newrelic.com/synthetics/api/v3/monitors"
+    url_file = "test_urls.txt"
+    js_file = 'test.js'
+    query = "name LIKE 'Domain Expiry'"
+    c = 0
 
     urls = synthetics_functions.get_urls(url_file)
-
-    monitors = synthetics_functions.get_monitors(headers, graph_url)
+    monitors = synthetics_functions.get_entities(graph_url, query, headers)
 
     for url in urls:
         clean_url = url.strip()
 
-        for i in range(len(urls)):
+        for i in range(len(monitors)):
             if clean_url:
-                guid = synthetics_functions.get_guid(i, clean_url, monitors)
+                ids = synthetics_functions.get_ids(i, clean_url, monitors)
             else:
                 print(f"{clean_url} not clean, trying next URL")
                 break
 
-            if guid is IndexError:
-                break
-            if guid is False:
+            if ids is IndexError:
                 i += 1
+            elif ids is False:
+                print(f"{clean_url} monitor not found")
+                break
             else:
-                mutation = synthetics_functions.get_update_script_monitor_mutation(
-                    guid, clean_url
-                )
+                monitor_id = ids[0]
+                encoded_script = synthetics_functions.create_domain_check_payload(js_file, clean_url)
+                payload = {"scriptText": encoded_script, "monitorId": monitor_id}
+                script_endpoint = f'{rest_url}/{monitor_id}/script'
+                break
 
-            response = synthetics_functions.make_graph_request(
-                graph_url, headers, mutation
-            )
+        response = synthetics_functions.make_rest_put_request(script_endpoint, headers, payload)
 
-        if '{"errors":[]}' in response.text and response.status_code == 200:
-            print(f"{response.status_code} : `{clean_url}` monitor updated")
+        if response.status_code == 204:
+            c += 1
+            print(f"{c}/{len(monitors)} : {response.status_code}/Ok : `{clean_url}` monitor updated")
         else:
             print(
                 f"{response.status_code} : `{clean_url}` monitor not updated : {response.text}"
