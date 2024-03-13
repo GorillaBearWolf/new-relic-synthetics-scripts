9514856d01ccb0381083544e6578f14a078faf6b
GorillaBearWolf <mrefsnider@icloud.com>
2024-03-12 21:27:18 -0400

    ♻️ Implemented new functions // Improved response messaging

--- create_domain_expiry_synthetics.py
+++ create_domain_expiry_synthetics.py
@@ -14,11 +14,12 @@ import synthetics_functions
 def main():
     load_dotenv()
 
-    url_file = "test_urls.txt"
-    graph_url = "https://api.newrelic.com/graphql"
     api_key = os.environ.get("NEW_RELIC_API_KEY")
     account_id = os.environ.get("NEW_RELIC_ACCOUNT_ID")
     headers = {"X-Api-Key": api_key, "Content-Type": "application/json"}
+    graph_url = "https://api.newrelic.com/graphql"
+    url_file = "test_urls.txt"
+    c = 0
 
     urls = synthetics_functions.get_urls(url_file)
 
@@ -29,12 +30,13 @@ def main():
             create_mutation = synthetics_functions.get_create_script_monitor_mutation(
                 clean_url, account_id
             )
-            response = synthetics_functions.make_graph_request(
+            response = synthetics_functions.make_graph_post_request(
                 graph_url, headers, create_mutation
             )
 
         if '{"errors":[]}' in response.text and response.status_code == 200:
-            print(f"{response.status_code} : `{clean_url}` monitor created")
+            c += 1
+            print(f"{c}/{len(urls)} : {response.status_code}/Ok : `{clean_url}` monitor created")
         else:
             print(
                 f"{response.status_code} : `{clean_url}` monitor not created : {response.text}"
--- delete_domain_expiry_synthetics.py
+++ delete_domain_expiry_synthetics.py
@@ -14,42 +14,48 @@ import synthetics_functions
 def main():
     load_dotenv()
 
-    url_file = "test_urls.txt"
     graph_url = "https://api.newrelic.com/graphql"
     api_key = os.environ.get("NEW_RELIC_API_KEY")
     headers = {"X-Api-Key": api_key, "Content-Type": "application/json"}
+    url_file = "test_urls.txt"
+    query = "name LIKE 'Domain Expiry'"
+    c = 0
+
 
     urls = synthetics_functions.get_urls(url_file)
-
-    monitors = synthetics_functions.get_monitors(headers, graph_url)
+    monitors = synthetics_functions.get_entities(graph_url, query, headers)
 
     for url in urls:
         clean_url = url.strip()
 
-        for i in range(len(urls)):
+        for i in range(len(monitors)):
             if clean_url:
-                guid = synthetics_functions.get_guid(i, clean_url, monitors)
+                ids = synthetics_functions.get_ids(i, clean_url, monitors)
             else:
                 print(f"{clean_url} not clean, trying next URL")
                 break
 
-            if guid is IndexError:
-                break
-            if guid is False:
+            if ids is IndexError:
                 i += 1
+            elif ids is False:
+                print(f"{clean_url} monitor not found")
+                break
             else:
+                guid = ids[1]
                 mutation = synthetics_functions.get_delete_monitor_mutation(guid)
+                break
 
-            response = synthetics_functions.make_graph_request(
-                graph_url, headers, mutation
+        response = synthetics_functions.make_graph_post_request(
+            graph_url, headers, mutation
+        )
+
+        if "deletedGuid" in response.text and response.status_code == 200:
+            c += 1
+            print(f"{c}/{len(monitors)} : {response.status_code}/Ok : `{clean_url}` monitor deleted")
+        else:
+            print(
+                f"{response.status_code} : `{clean_url}` monitor not deleted : {response.text}"
             )
-
-            if "deletedGuid" in response.text and response.status_code == 200:
-                print(f"{response.status_code} : `{clean_url}` monitor deleted")
-            else:
-                print(
-                    f"{response.status_code} : `{clean_url}` monitor not deleted : {response.text}"
-                )
 
     print("All found URLs have been processed.")
 
